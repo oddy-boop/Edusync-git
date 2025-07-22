4c5eea6e8022000fca179f5e06e769414c6cb9dd
Richard Odoom <odoomrichard089@gmail.com>
2025-07-22 19:01:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -30,6 +30,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import {
   Select,
@@ -690,3 +691,5 @@ export default function AdminUsersPage() {
     </div>
   );
 }
+
+    
