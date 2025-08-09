093a72c248277265ef2b56baffd1fb0c86b96dd5
Richard Odoom <odoomrichard089@gmail.com>
2025-08-09 19:54:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -681,7 +681,7 @@ export default function AdminSettingsPage() {
                             </div>
                              <div style={{ height: '300px', width: '100%' }} className="rounded-lg overflow-hidden">
                                 {typeof window !== 'undefined' && (
-                                <MapContainer center={mapPosition} zoom={16} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+                                <MapContainer key={`${mapPosition[0]}-${mapPosition[1]}`} center={mapPosition} zoom={16} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
                                     <TileLayer
                                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
